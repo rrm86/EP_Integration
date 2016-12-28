3594114e82f51e3d4f1d723a187df7f55b16bc9c
Ronnald Rezende Machado <rrmachado@id.uff.br>
2016-12-28 12:09:10 -0200

    version 0.1

--- integration.py
+++ integration.py
@@ -10,20 +10,55 @@ def connection(database):
     return db
 
 
-def addCourse(dbEP):
+def addCourse(dbEP,dbMoodle):
     db = connection(dbEP)
     cursor = db.cursor()
-    with open('teste.csv', 'rb') as f:
+    with open('ExportaCurso.csv', 'rb') as f:
         reader = csv.reader(f)
         for row in reader:
             try:
+                handleCategory(dbMoodle,row[2])
+                category = idParent(dbMoodle,row[2])
                 cursor.execute('INSERT INTO course values("%s","%s","%s","%s")' % \
-                (row[0], row[0],row[1],row[2]))
+                (row[0], row[0],row[1],category))
                 db.commit()
             except Exception as e:
                 print e
                 db.rollback()
     db.close()
+
+def handleCategory(dbMoodle,category):
+    db = connection(dbMoodle)
+    cursor = db.cursor()
+    getLastId = db.cursor()
+    getPath = db.cursor()
+    newCat = db.cursor()
+    cursor.execute('''SELECT id FROM mdl_course_categories Where name = %s''',(category))
+    categoryId= cursor.fetchone()
+    #check if category exist
+    if  categoryId is not None:
+        db.commit()
+        db.close()
+    else:
+        getLastId.execute('''SELECT MAX(id) FROM mdl_course_categories''')
+        newId = int(getLastId.fetchone()[0])+1
+        newId = str(newId)
+        getPath.execute('''SELECT path FROM mdl_course_categories Where idnumber = %s''',(category[4:]))
+        path = str(getPath.fetchone()[0])
+        parent = int(path[1:])
+        path = path+"/"+newId
+        newCat.execute('''INSERT INTO mdl_course_categories (name,idnumber,parent,timemodified,depth,path)values(%s,%s,%s,%s,%s,%s)''',(category,category,parent,int(time.time()),2,path))
+        db.commit()
+        db.close()
+
+
+def idParent(dbMoodle,category):
+    db = connection(dbMoodle)
+    cursor = db.cursor()
+    cursor.execute('''SELECT id FROM mdl_course_categories Where name = %s''',(category))
+    parentId = cursor.fetchone()[0]
+    return parentId
+
 
 def addEnrolment(dbEP):
     db = connection(dbEP)
@@ -64,11 +99,10 @@ def addUser(dbMoodle):
                 db.rollback()
     db.close()
 
-
-
 param={"dbExtra":"escolaparque","moodle":"moodletesteEP"}
 renameFile()
-addCourse(param["dbExtra"])
+addCourse(param["dbExtra"],param["moodle"])
 addEnrolment(param["dbExtra"])
 addUser(param["moodle"])
+
 print "ok"
